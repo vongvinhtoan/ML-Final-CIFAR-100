69539250f2d081d6e66f098ec33dd13a1708bd6a
Naot <toanvv.ti.1922@gmail.com>
2025-08-27 07:42:34 +0700

    feat: remove EfficientNetL2 and add EfficientNetV2_L, EfficientNetV2_M, and EfficientNet_B7 models with updated sweep configurations

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,10 +1,14 @@
 from torch import nn
 from .resnet import Resnet
-from .effnet_l2 import EfficientNetL2
+from .effnetv2_l import EfficientNetV2_L
+from .effnetv2_m import EfficientNetV2_M
+from .effnet_b7 import EfficientNet_B7
 from typing import Type
 
 
 models: dict[str, Type[nn.Module]] = {
     'resnet': Resnet,
-    'effnet_l2': EfficientNetL2
+    'effnetv2_l': EfficientNetV2_L,
+    'effnetv2_m': EfficientNetV2_M,
+    'effnet_b7': EfficientNet_B7
 }
---
+++ src/model/effnet_b7.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch.nn.init as init
+from torchvision import models
+
+class EfficientNet_B7(nn.Module):
+    def __init__(self, freeze_pretrained=False, weight_init=False):
+        super().__init__()
+
+        # Load EfficientNet-V2-M with pretrained weights
+        self.model = models.efficientnet_b7(
+            weights=models.EfficientNet_B7_Weights.DEFAULT
+        )
+
+        # Modify classifier for CIFAR-100
+        in_features: int = self.model.classifier[-1].in_features  # type: ignore
+        fc = nn.Linear(in_features, 100)
+        if weight_init:
+            init.xavier_uniform_(fc.weight)
+            if fc.bias is not None:
+                init.zeros_(fc.bias)
+        self.model.classifier[-1] = fc
+
+        # Optionally freeze all pretrained layers except first conv and classifier
+        if freeze_pretrained:
+            for name, param in self.model.named_parameters():
+                if not name.startswith("features.0.0") and not name.startswith("classifier"):
+                    param.requires_grad = False
+
+    def forward(self, x):
+        return self.model(x)
