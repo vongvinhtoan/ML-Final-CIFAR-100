543dfe5a1163b22be70e463f0945e26505802fcf
Naot <toanvv.ti.1922@gmail.com>
2025-09-12 22:52:52 +0700

    feat: modify SmallCNN architecture by adjusting convolutional layer parameters and classifier dimensions

--- src/model/cnn.py
+++ src/model/cnn.py
@@ -10,23 +10,23 @@ class SmallCNN(nn.Module):
         weight_init = kwargs['weight_init']
 
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),  # 3x32x32 -> 32x32x32
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),  # 3x32x32 -> 32x32x32
             nn.ReLU(),
             nn.MaxPool2d(2),  # -> 32x16x16
 
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),  # -> 64x16x16
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),  # -> 64x16x16
             nn.ReLU(),
             nn.MaxPool2d(2),  # -> 64x8x8
 
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),  # -> 128x8x8
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),  # -> 128x8x8
             nn.ReLU(),
             nn.MaxPool2d(2),  # -> 128x4x4
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(128 * 4 * 4, 256),
+            nn.Linear(64 * 4 * 4, 128),
             nn.ReLU(),
-            nn.Linear(256, 100)
+            nn.Linear(128, 100)
         )
 
         if weight_init:
